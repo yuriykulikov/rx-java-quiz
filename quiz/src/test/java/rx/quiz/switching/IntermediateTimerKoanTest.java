28c8ec4b0380cc67ef49bfd68e4a09a503f2549d
ihula <ihula@luxoft.com>
2019-06-05 15:53:42 +0300

    Fix IntermediateTimerKoanTest. Use timer and concat instead of scheduleDirect. Because recursive scheduleDirect won't dispose.

--- quiz/src/test/java/rx/quiz/switching/IntermediateTimerKoanTest.java
+++ quiz/src/test/java/rx/quiz/switching/IntermediateTimerKoanTest.java
@@ -1,7 +1,6 @@
 package rx.quiz.switching;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
 import io.reactivex.Scheduler;
 import io.reactivex.annotations.NonNull;
 import org.jetbrains.annotations.NotNull;
@@ -13,13 +12,11 @@ public class IntermediateTimerKoanTest extends IntermediateTimerKoan {
     @NotNull
     @Override
     public Observable<Long> whoaSlowDown(@NotNull Scheduler scheduler) {
-        return Observable.create(emitter -> emitValues(emitter, scheduler, 3L));
+        return emitValues(scheduler, 3L);
     }
 
-    private void emitValues(@NonNull ObservableEmitter<Long> emitter, @NotNull Scheduler scheduler, @NonNull Long delay) {
-        scheduler.scheduleDirect(() -> {
-            emitter.onNext(0L);
-            emitValues(emitter, scheduler, delay + 1);
-        }, delay, TimeUnit.SECONDS);
+    private Observable<Long> emitValues(@NotNull Scheduler scheduler, @NonNull Long delay) {
+        return Observable.concat(Observable.timer(delay, TimeUnit.SECONDS, scheduler),
+                Observable.defer(() -> emitValues(scheduler, delay + 1)));
     }
 }
