2cd0c43c8c150a39803018cefcd40a8ee8f67224
ihula <ihula@luxoft.com>
2019-06-06 12:24:31 +0300

    Fix IntermediateTimeKoanTest - don't use Observable.concat with recursion, because it won't be disposed

--- quiz/src/test/java/rx/quiz/switching/IntermediateTimerKoanTest.java
+++ quiz/src/test/java/rx/quiz/switching/IntermediateTimerKoanTest.java
@@ -2,21 +2,24 @@ package rx.quiz.switching;
 
 import io.reactivex.Observable;
 import io.reactivex.Scheduler;
-import io.reactivex.annotations.NonNull;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class IntermediateTimerKoanTest extends IntermediateTimerKoan {
 
     @NotNull
     @Override
     public Observable<Long> whoaSlowDown(@NotNull Scheduler scheduler) {
-        return emitValues(scheduler, 3L);
-    }
+        AtomicLong delay = new AtomicLong(3L);
 
-    private Observable<Long> emitValues(@NotNull Scheduler scheduler, @NonNull Long delay) {
-        return Observable.concat(Observable.timer(delay, TimeUnit.SECONDS, scheduler),
-                Observable.defer(() -> emitValues(scheduler, delay + 1)));
+        return Observable.create(emitter -> scheduler.scheduleDirect(new Runnable() {
+            @Override
+            public void run() {
+                emitter.onNext(0L);
+                scheduler.scheduleDirect(this, delay.getAndIncrement(), TimeUnit.SECONDS);
+            }
+        }, delay.getAndIncrement(), TimeUnit.SECONDS));
     }
 }
